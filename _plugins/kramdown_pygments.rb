d3a68e6ae4e4a9c8018ad2dcc5dd0697e2cd6820
Wulfric Wang <lufeihaidao+code@gmail.com>
2014-09-15 20:59:31 +0800

    update

--- _plugins/kramdown_pygments.rb
+++ _plugins/kramdown_pygments.rb
@@ -1,26 +1,5 @@
-
-# This class is the actual custom Jekyll converter.
-class Jekyll::Converters::Markdown::KramdownPygments
-
-  def initialize(config)
-    require 'kramdown'
-    @config = config
-  rescue LoadError
-    STDERR.puts 'You are missing a library required for Markdown. Please run:'
-    STDERR.puts '  $ [sudo] gem install kramdown'
-    raise FatalException.new("Missing dependency: kramdown")
-  end
-
-  def convert(content)
-    html = Kramdown::Document.new(content, {
-        :auto_ids             => @config['kramdown']['auto_ids'],
-        :footnote_nr          => @config['kramdown']['footnote_nr'],
-        :entity_output        => @config['kramdown']['entity_output'],
-        :toc_levels           => @config['kramdown']['toc_levels'],
-        :smart_quotes         => @config['kramdown']['smart_quotes'],
-        :kramdown_default_lang => @config['kramdown']['default_lang'],
-        :input                => @config['kramdown']['input']
-    }).to_html
-    return html
+class Kramdown::Converter::Html
+  def convert_img(el, indent)
+    "<em><img#{html_attributes(el.attr)} /></em>"
   end
 end

adcfcd7ff134d16051bd5fb7c4a6f260ea32c8cd
Wulfric Wang <lufeihaidao+code@gmail.com>
2014-09-26 00:57:29 +0800

    u

--- _plugins/categories.rb
+++ _plugins/categories.rb
@@ -1,18 +1,24 @@
 module Jekyll
 
   class CategoryIndex < Page
-    def initialize(site, base, dir, category)
+    def initialize(site, base, dir, cat_value, is_tag)
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
 
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
-      self.data['category'] = category
+      if is_tag
+        cat_name = 'tag'
+      else
+        cat_name = 'category'
+      end
 
-      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
-      self.data['title'] = "#{category_title_prefix}#{category}"
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), "#{cat_name}_index.html")
+      self.data[cat_name] = cat_value
+
+      cat_name_title_prefix = site.config["#{cat_name}_title_prefix"] || "#{cat_name.capitalize}: "
+      self.data['title'] = "#{cat_name_title_prefix}#{cat_value}"
     end
   end
 
@@ -23,13 +29,21 @@ module Jekyll
       if site.layouts.key? 'category_index'
         dir = site.config['category_dir'] || 'categories'
         site.categories.keys.each do |category|
-          write_category_index(site, File.join(dir, category), category)
+          write_category_index(site, File.join(dir, category), category, false)
         end
       end
+
+      if site.layouts.key? 'tag_index'
+        dir = site.config['tag_dir'] || 'tags'
+        site.tags.keys.each do |tag|
+          write_category_index(site, File.join(dir, tag), tag, true)
+        end
+      end
+
     end
 
-    def write_category_index(site, dir, category)
-      index = CategoryIndex.new(site, site.source, dir, category)
+    def write_category_index(site, dir, category, is_tag)
+      index = CategoryIndex.new(site, site.source, dir, category, is_tag)
       index.render(site.layouts, site.site_payload)
       index.write(site.dest)
       site.pages << index

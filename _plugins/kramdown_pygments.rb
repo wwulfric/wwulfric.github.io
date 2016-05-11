0d063ad00e43ab807a11f82d53988cc76671ebd0
Wulfric Wang <lufeihaidao+code@gmail.com>
2016-05-11 21:06:40 +0800

    2016-05-11 21:06

--- _plugins/kramdown_pygments.rb
+++ _plugins/kramdown_pygments.rb
@@ -4,51 +4,96 @@ module Kramdown
   module Converter
     class Html
       begin
-        require 'pygments'
-        require 'mini_magick'
+        require 'json'
+        require 'net/http'
       rescue LoadError
         STDERR.puts 'You are missing a library required for syntax highlighting. Please run:'
-        STDERR.puts '  $ [sudo] gem install pygments mini_magick'
         raise FatalException.new("Missing dependency: Pygments")
       end
 
       def convert_codeblock(el, indent)
         attr = el.attr.dup
         lang = extract_code_language!(attr)
-        code = pygmentize(el.value, lang)
-        code_attr = {}
-        code_attr['class'] = "language-#{lang}" if lang
-        "#{' '*indent}<div class=\"highlight\"><pre#{html_attributes(attr)}><code#{html_attributes(code_attr)}>#{code}</code></pre></div>\n"
+        hl_opts = {}
+        highlighted_code = highlight_code(el.value, el.options[:lang] || lang, :block, hl_opts)
+
+        if highlighted_code
+          add_syntax_highlighter_to_class_attr(attr, lang || hl_opts[:default_lang])
+          "#{' '*indent}<div #{html_attributes(attr)}>#{highlighted_code}#{' '*indent}</div>\n"
+        else
+          result = escape_html(el.value)
+          result.chomp!
+          if el.attr['class'].to_s =~ /\bshow-whitespaces\b/
+            result.gsub!(/(?:(^[ \t]+)|([ \t]+$)|([ \t]+))/) do |m|
+              suffix = ($1 ? '-l' : ($2 ? '-r' : ''))
+              m.scan(/./).map do |c|
+                case c
+                when "\t" then "<span class=\"ws-tab#{suffix}\">\t</span>"
+                when " " then "<span class=\"ws-space#{suffix}\">&#8901;</span>"
+                end
+              end.join('')
+            end
+          end
+          code_attr = {}
+          code_attr['class'] = "language-#{lang}" if lang
+          "#{' '*indent}<pre#{html_attributes(attr)}><code#{html_attributes(code_attr)}>#{result}\n</code></pre>\n"
+        end
       end
 
+      # def convert_codeblock(el, indent)
+      #   attr = el.attr.dup
+      #   lang = extract_code_language!(attr)
+      #   code = pygmentize(el.value, lang)
+      #   code_attr = {}
+      #   code_attr['class'] = "language-#{lang}" if lang
+      #   "#{' '*indent}<div class=\"highlight\"><pre#{html_attributes(attr)}><code#{html_attributes(code_attr)}>#{code}</code></pre></div>\n"
+      # end
+
+      # def convert_codespan(el, indent)
+      #   attr = el.attr.dup
+      #   lang = extract_code_language!(attr)
+      #   code = pygmentize(el.value, lang)
+      #   if lang
+      #     attr['class'] = "highlight"
+      #     if attr.has_key?('class')
+      #       attr['class'] += " language-#{lang}"
+      #     else
+      #       attr['class'] = "language-#{lang}"
+      #     end
+      #   end
+      #   "<span#{html_attributes(attr)} class='codespan'>#{code}</span>"
+      # end
       def convert_codespan(el, indent)
         attr = el.attr.dup
-        lang = extract_code_language!(attr)
-        code = pygmentize(el.value, lang)
-        if lang
-          attr['class'] = "highlight"
-          if attr.has_key?('class')
-            attr['class'] += " language-#{lang}"
-          else
-            attr['class'] = "language-#{lang}"
-          end
+        lang = extract_code_language(attr)
+        hl_opts = {}
+        result = highlight_code(el.value, lang, :span, hl_opts)
+        if result
+          add_syntax_highlighter_to_class_attr(attr, hl_opts[:default_lang])
+        else
+          result = escape_html(el.value)
         end
-        "<span#{html_attributes(attr)} class='codespan'>#{code}</span>"
+        attr['class'] = 'codespan'
+        format_as_span_html('span', attr, result)
       end
 
       def convert_img(el, indent)
-        image = MiniMagick::Image.open el.attr['src']
-        width = image[:width]
-        height = image[:height]
-        unless el.attr['src'].match(/R-/).nil?
-          width /= 2
-          height /= 2
-        end
-        img_max_width = 720
-        if width > img_max_width
-          ratio = width.to_f / img_max_width
-          width = img_max_width
-          height = (height / ratio).to_i
+        src = el.attr['src']
+        if src['qiniudn']
+          image_info_uri = URI "#{src}?imageInfo"
+          image_info = JSON.parse(Net::HTTP.get image_info_uri)
+          width = image_info['width']
+          height = image_info['height']
+          unless src.match(/R-/).nil?
+            width /= 2
+            height /= 2
+          end
+          img_max_width = 720
+          if width > img_max_width
+            ratio = width.to_f / img_max_width
+            width = img_max_width
+            height = (height / ratio).to_i
+          end
         end
         unless el.attr['alt'].empty?
           figcaption = "<figcaption><i class='icon-pencil'></i>#{el.attr['alt']}</figcaption>"
@@ -61,15 +106,6 @@ module Kramdown
         </figure>"
       end
 
-      def pygmentize(code, lang)
-        if lang
-          Pygments.highlight(code,
-            :lexer => lang,
-            :options => { :startinline => true, :encoding => 'utf-8', :nowrap => true })
-        else
-          escape_html(code)
-        end
-      end
     end
   end
 end
